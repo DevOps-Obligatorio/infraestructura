82f7ced521237a724a5272ada630672c9227c49e
Damian <dlaureiro@outlook.com>
2022-07-02 15:47:26 -0300

    terraform

---
+++ main.tf
@@ -0,0 +1,129 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+#### CREATE VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.10.0.0/16"
+  
+  tags = {
+    Name = "main"
+  }  
+}
+#### CREATE SUBNET
+resource "aws_subnet" "principal" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.10.10.0/24"
+  availability_zone_id = "use1-az1"
+  
+  tags = {
+     Name = "main" 
+  }
+}
+resource "aws_subnet" "secundaria" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.10.20.0/24"
+  availability_zone_id = "use1-az2"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+#### CREATE SECURITY GROUP
+resource "aws_security_group" "vpc-sg" {
+  name = "main"
+  vpc_id = aws_vpc.main.id
+  description = "VPC Default Security Group"
+
+  tags = {
+    Name = "main"
+  }
+
+  ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#### CREATE CLUSTER ECS 
+resource "aws_ecs_cluster" "main" {
+  name = "myapp-cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "myapp-log" {
+  name              = "myapp-log"
+  retention_in_days = 30
+}
+
+####TASK DEFINITION 
+resource "aws_ecs_task_definition" "main" {
+    family                   = "myapp-task"
+    requires_compatibilities = ["FARGATE"]
+    network_mode             = "awsvpc"
+    cpu                      = "1024"
+    memory                   = "2048"
+    execution_role_arn       =  "arn:aws:iam::450890513155:role/LabRole"
+    container_definitions    = jsonencode([
+        {
+            name         =     "product-service"
+            image        =     "nginx:latest"
+            #cpu          =     256
+            memory       =     512
+            essentials   =     true
+            portMappings = [
+                {
+                    containerPort = 80  
+                    hostPort      = 80
+                }
+            ]
+            logConfiguration = {
+                    logDriver = "awslogs"
+                    options =  {
+                            awslogs-group = "myapp-log"
+                            awslogs-region  = "us-east-1"
+                            awslogs-stream-prefix = "ecs"
+                    }
+            }
+            } 
+    ])
+}
+
+####SERVICE DEFINITION
+resource "aws_ecs_service" "main" {
+  name            = "myapp-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.main.arn
+  #health_check_grace_period_seconds = 2
+  deployment_minimum_healthy_percent = 1
+  deployment_maximum_percent = 100
+  desired_count   = 2
+  launch_type     = "FARGATE"
+
+ network_configuration {
+    security_groups  = [aws_security_group.vpc-sg.id]
+    subnets          = aws_subnet.principal.*.id
+    assign_public_ip = true
+  }    
+}
